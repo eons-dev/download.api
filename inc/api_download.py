a863b09c671224497d80b70b66ed1c7c8fafb21c
eons <eons@eons.dev>
2022-09-26 21:35:10 -0700

    test and debug

--- inc/api_download.py
+++ inc/api_download.py
@@ -9,6 +9,10 @@ class download(external):
     def __init__(this, name="download"):
         super().__init__(name)
 
+        # We want to send binary data.
+        this.mime = 'application/force-download'
+        this.decode = None
+        
         this.supportedMethods = ['GET']
 
         # The redirect_url_field is used if the first request *this makes returns a json describing the file to be downloaded. In such a case, a second request is made to the url provided by the field specified in the json of the response to the first request.
@@ -27,14 +31,14 @@ Per the parent 'external':
 
         #TODO: does stream=True work? Can we pass a stream through requests to flask?
 
-        if (this.redirect_url_field is None):
+        if (not this.redirect_url_field):
             this.externalResponse = requests.request(**this.externalRequest, stream=True)
             return
 
         firstResponse = requests.request(**this.externalRequest)
         frJson = json.loads(firstResponse.content.decode('ascii'))
 
-        this.externalResponse = requests.get(frJson[this.redirect_url_field], stream=True)
+        this.externalResponse = requests.get(frJson[0][this.redirect_url_field], stream=True)
 
     # All we're doing here is setting a 2-stage download process.
     # We can use the external Call() method.
